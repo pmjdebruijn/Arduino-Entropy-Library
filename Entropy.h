pmjdebruijn/Arduino-Entropy-Library

commit aadd30945e98a96d171a554ddf7708998da37782

include specific headers

Entropy.h
@@ -19,7 +19,8 @@
 #ifndef Entropy_h
 #define Entropy_h
 
-#include <stdint.h>
+#include <string.h>
+#include <math.h>
 
 // Separate the ARM Due headers we use
 #ifdef ARDUINO_SAM_DUE

Entropy.cpp
@@ -21,7 +21,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Entropy.  If not, see <http://www.gnu.org/licenses/>.
 
-#include <Arduino.h>
 #include <Entropy.h>
 
 const uint8_t WDT_MAX_8INT=0xFF;

commit 67019ffb6a4e11bf350668a53c051f9171bf0ad7

Fix Due support
https://github.com/pmjdebruijn/Arduino-Entropy-Library/commit/aadd30945e98a96d171a554ddf7708998da37782 broke support for the Arduino Due by removing the #include directive for Arduino.h:

E:\arduino\libraries\Arduino-Entropy-Library\Entropy.cpp: In member function 'void EntropyClass::initialize()':

E:\arduino\libraries\Arduino-Entropy-Library\Entropy.cpp:62:32: error: 'pmc_enable_periph_clk' was not declared in this scope

   pmc_enable_periph_clk(ID_TRNG);

In the spirit of that commit, I have added the #include directive for the specific header file that contains the declaration of pmc_enable_periph_clk.

Entropy.h
@@ -26,6 +26,7 @@
 #ifdef ARDUINO_SAM_DUE
 #include <sam.h>
 #include <sam3xa/include/component/component_trng.h>
+#include <include/pmc.h>
 #endif
 
 // Teensy required headers